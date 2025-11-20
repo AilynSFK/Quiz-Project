224d5ebe3e14f8d12808702f05317c841d843c0b
Ailyn <ailynshaffak@gmail.com>
2025-11-20 23:44:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "Berlin", correct: false },
+            { text: "Amsterdam", correct: false },
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+        ]
+    },
+
+    {
+        question: "What is the capital of United States Of America?",
+        answers: [
+            { text: "Phoenix", correct: false },
+            { text: "Amsterdam", correct: false },
+            { text: "Wasshington Dc", correct: true },
+            { text: "Paris", correct: false },
+        ]
+    },
+
+    {
+        question: "What is The Biggest Country in The World?",
+        answers: [
+            { text: "China", correct: false },
+            { text: "Japan", correct: false },
+            { text: "Russia", correct: true },
+            { text: "United States Of America", correct: false },
+        ]
+    },
+
+    {
+        question: "What is Country That Has The Most Population?",
+        answers: [
+            { text: "India", correct: true },
+            { text: "Indonesia", correct: false },
+            { text: "China", correct: false },
+            { text: "Pakistan", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You score ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=> {
+    if(currentQuestionIndex < questions.length) {
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
